96e57ab0651df54c74334da6270591a84a584425
BritishPandaChick <sbartley1080@gmail.com>
2020-02-14 19:16:12 -0500

    Create about, advice, books, and media pages. Update code for header and footer partial files

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.get("/advice", function(req, res){
+    res.render("advice");
+});
+
+app.get("/books", function(req, res){
+    res.render("books");
+});
+
+app.get("/media", function(req, res){
+    res.render("media");
+});
+
 app.listen(port, function(){
     console.log("Jane is now live on the server");
 });
